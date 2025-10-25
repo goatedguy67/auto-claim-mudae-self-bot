98c6016731d177856e6a91f0304fc750dddd695f
bignoobgoob <pegasusboy02@gmail.com>
2025-10-24 22:14:03 -0600

    Finished initial Cooldown.py

--- src/Cooldown.py
+++ src/Cooldown.py
@@ -3,15 +3,34 @@ from asyncio import sleep
 
 
 class Cooldown:
-    def __init__(self):
-        self.in_cooldown: bool = False
-        self.cooldown_time: float = 0.0
+    async def __init__(self, cooldown: float = 0, max_cooldown: float = 86000):
+        """
+        Initializes a Cooldown instance.
+        If a cooldown time is provided, it sets the cooldown immediately.
+        :param cooldown: The initial cooldown time in seconds.
+        :param max_cooldown: The maximum cooldown time in seconds.
+        """
 
-    async def set_cooldown(self, tempo: float) -> None:
-        self.in_cooldown = True
+        if cooldown:
+            await self.set_cooldown(cooldown)
+
+        self.on_cooldown: bool = False
+        self.cooldown_time: float = 0.0
+        self.max_cooldown: float = max_cooldown
+
+    async def set_cooldown(self, tempo: float = 0) -> None:
+        """
+        Sets the cooldown for the specified duration if one is provided
+        else uses the maximum cooldown time.
+        """
+
+        if tempo == 0:
+            tempo = self.max_cooldown
+
+        self.on_cooldown = True
         self.cooldown_time = time.time() + tempo
         await sleep(tempo)
-        self.in_cooldown = False
+        self.on_cooldown = False
 
     def get_current_cooldown(self) -> float:
         return self.cooldown_time - time.time()
